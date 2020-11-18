1d27b1f5f580dd1ae4f7000c1593ccd0be1fa595
yuta ohira <alesion90@gmail.com>
2020-11-18 21:00:39 +0900

    現在の室温を取得するAPIを追加

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require("express");
 const app = express();
 const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
+
+// Corresponding CORS(Cross-Origin Resource Sharing)
+const cors = require("cors");
+app.use(cors());
 
 // config
 const port = 3001;
 const arduinoCOMPort = "/dev/cu.usbmodem141401";
 
 // Arduino Serial
-const arduinoSerialPort = new SerialPort(arduinoCOMPort, 9600);
-arduinoSerialPort.on("open", function () {
+const parser = new Readline();
+const arduinoSerial = new SerialPort(arduinoCOMPort);
+arduinoSerial.pipe(parser);
+
+// シリアル通信 開始時
+parser.on("open", function () {
   console.log("Serial Port " + arduinoCOMPort + " is opened.");
 });
-arduinoSerialPort.on("data", (data) => {
-  console.log("Data: " + data);
+
+// シリアル通信 受信時
+let currentTemp = 0;
+parser.on("data", (data) => {
+  // 送られてきたデータが数値だったら、現在の室温に反映
+  let castData = Number(data);
+  if (!isNaN(castData)) {
+    console.log("temp: " + castData);
+    currentTemp = castData;
+  } else {
+    console.log("data: " + data);
+  }
 });
 
 ////////////////////////////////////////////////////////////////
@@ -22,16 +41,22 @@ app.get("/", function (req, res) {
   return res.send("Working");
 });
 
-// action LED制御
-app.get("/:action", function (req, res) {
-  var action = req.params.action;
+// 現在の室温を取得
+app.get("/temp", function (req, res) {
+  return res.send(String(currentTemp));
+});
 
+// Arduinoの制御
+app.get("/control/:action", function (req, res) {
+  const action = req.params.action; // actionを受け取る
+
+  // actionによって、Arduinoを制御
   if (action == "led") {
-    arduinoSerialPort.write("w");
+    arduinoSerial.write("w");
     return res.send("Led light is on!");
   }
   if (action == "off") {
-    arduinoSerialPort.write("t");
+    arduinoSerial.write("t");
     return res.send("Led light is off!");
   }
 
@@ -41,5 +66,5 @@ app.get("/:action", function (req, res) {
 ////////////////////////////////////////////////////////////////
 
 app.listen(port, function () {
-  console.log("Example app listening on port http://localhost:" + port + "!");
+  console.log("listening on port http://localhost:" + port + "!");
 });
