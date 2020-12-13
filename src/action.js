060673fc2047df1e31bf8e448cf1735f3f0c54fe
yuto <tanaka.yuto.3002@gmail.com>
2020-12-13 16:57:46 +0900

    エアコン起動・停止のaction追加

--- src/action.js
+++ src/action.js
@@ -6,12 +6,19 @@ exports.index = (req, res) => {
 
   // Arduinoにactionを送信
   switch (action) {
-    case "led":
-      arduinoSerial.write("w");
-      return res.send("Led light is on!");
+    // case "led":
+    //   arduinoSerial.write("w");
+    //   return res.send("Led light is on!");
+    // case "off":
+    //   arduinoSerial.write("t");
+    //   return res.send("Led light is off!");
+    case "on":
+      //Power, Mode, Fan(風量), Temp, Vdir(風向高さ), Hdir(風向左右)
+      arduinoSerial.write('P1M2F0T4V0H0E');
+      return res.send("Air conductor ON!");
     case "off":
-      arduinoSerial.write("t");
-      return res.send("Led light is off!");
+      arduinoSerial.write("O");
+      return res.send("Air conductor OFF!");
     default:
       return res.send("Action: " + action);
   }
