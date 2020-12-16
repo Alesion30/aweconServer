c6a6b6e9f320cdd9973dd9de098e514eec1551e8
yuto <tanaka.yuto.3002@gmail.com>
2020-12-16 17:17:48 +0900

    arduinoへの送信を一文字ずつに

--- src/action.js
+++ src/action.js
@@ -6,15 +6,22 @@ exports.index = (req, res) => {
 
   // Arduinoにactionを送信
   switch (action) {
-    // case "led":
-    //   arduinoSerial.write("w");
-    //   return res.send("Led light is on!");
-    // case "off":
-    //   arduinoSerial.write("t");
-    //   return res.send("Led light is off!");
     case "on":
-      //Power, Mode, Fan(風量), Temp, Vdir(風向高さ), Hdir(風向左右)
-      arduinoSerial.write('P1M2F0T4V0H0E');
+      //arduinoにエアコン起動に必要な情報を送信
+      //Power(電源), Mode, Fan(風量), Temp(20度台下一桁),{t(10度台)}, Vdir(風向高さ), Hdir(風向左右)
+      arduinoSerial.write('P');
+      arduinoSerial.write('1');
+      arduinoSerial.write('M');
+      arduinoSerial.write('2');
+      arduinoSerial.write('F');
+      arduinoSerial.write('0');
+      arduinoSerial.write('T');
+      arduinoSerial.write('4');
+      arduinoSerial.write('V');
+      arduinoSerial.write('0');
+      arduinoSerial.write('H');
+      arduinoSerial.write('0');
+      arduinoSerial.write('E');
       return res.send("Air conductor ON!");
     case "off":
       arduinoSerial.write("O");
