dcd6deb5b23b231b1c5d878eef9fbf205213a9c7
yuta ohira <alesion90@gmail.com>
2020-12-21 21:22:55 +0900

    返り値を修正

--- src/action.js
+++ src/action.js
@@ -1,4 +1,4 @@
-const arduino = require("./arduino.js");
+const arduino = require('./arduino.js');
 const arduinoSerial = arduino.arduinoSerial;
 
 exports.index = (req, res) => {
@@ -6,7 +6,7 @@ exports.index = (req, res) => {
 
   // Arduinoにactionを送信
   switch (action) {
-    case "on":
+    case 'on':
       const { mode, fan, temp, vdir, hdir } = req.query;
 
       // エラーハンドリング
@@ -20,38 +20,43 @@ exports.index = (req, res) => {
       let tempCode; // 16~19度 -> t, 20~30度 -> T
       let tempDigit; // 温度下一桁
       if (req.query.temp < 20) {
-        tempCode = "t";
+        tempCode = 't';
         tempDigit = req.query.temp - 10;
         tempDigit = String(tempDigit);
       } else {
-        tempCode = "T";
+        tempCode = 'T';
         tempDigit = req.query.temp - 20;
         tempDigit = String(tempDigit);
       }
 
       //arduinoにエアコン起動に必要な情報を送信
       //Mode(運転モード)
-      arduinoSerial.write("M");
+      arduinoSerial.write('M');
       arduinoSerial.write(mode);
       //Fan(風量),
-      arduinoSerial.write("F");
+      arduinoSerial.write('F');
       arduinoSerial.write(fan);
       //Temp(20度台下一桁),{t(10度台)},
       arduinoSerial.write(tempCode);
       arduinoSerial.write(tempDigit);
       //Vdir(風向高さ)
-      arduinoSerial.write("V");
+      arduinoSerial.write('V');
       arduinoSerial.write(vdir);
       //Hdir(風向左右)
-      arduinoSerial.write("H");
+      arduinoSerial.write('H');
       arduinoSerial.write(hdir);
       //End(終了信号)
-      arduinoSerial.write("E");
-      return res.send("Air conductor ON!");
-    case "off":
-      arduinoSerial.write("O");
-      return res.send("Air conductor OFF!");
+      arduinoSerial.write('E');
+
+      const json = {
+        message: 'Successfully activated the air conditioner!!',
+        query: req.query
+      };
+      return res.json(json);
+    case 'off':
+      arduinoSerial.write('O');
+      return res.send('Successfully deactivated the air conditioner...');
     default:
-      return res.send("Action: " + action);
+      return res.send('Action: ' + action);
   }
 };
